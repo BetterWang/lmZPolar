7347b6de8394fd9fef02489259a5193ef9e64ed1
Quan Wang <BetterWang@gmail.com>
2021-09-11 03:22:33 +0200

    [+] update

--- synclx.sh
+++ synclx.sh
@@ -1,1 +1,1 @@
-rsync -avz -e ssh qwang@lxplus:/afs/cern.ch/user/q/qwang/work/cleanroomRun7/Ana/CMSSW_10_3_3_patch1/src/QWAna/QWLmZPolar/bin/txt .
+rsync -avz --info=progress2 -e ssh qwang@lxplus:/afs/cern.ch/user/q/qwang/work/cleanroomRun7/Ana/CMSSW_10_3_3_patch1/src/QWAna/QWLmZPolar/bin/txt .
